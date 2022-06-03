b15ae6eb0ea67d7f6ec71e83fd1426473c665632
Stargirl-chan <henkeben0@gmail.com>
2022-06-03 18:00:04 +0200

    added gtk4, helvum

---
+++ gnome/gtk4/build.sh
@@ -0,0 +1,52 @@
+pkgname=gtk
+_pkgver=4.7
+pkgver=$_pkgver.0
+
+fetch() {
+	curl -L "https://download.gnome.org/sources/$pkgname/$_pkgver/$pkgname-$pkgver.tar.xz" -o $pkgname-$pkgver.tar.xz
+	tar -xf $pkgname-$pkgver.tar.xz
+	mkdir -p $pkgname-$pkgver/_build
+	cd $pkgname-$pkgver
+	patch -p1 < ../../fix-llvm-build.patch
+
+}
+
+build() {
+	cd $pkgname-$pkgver
+	meson _build \
+		--buildtype=release \
+		--prefix=/usr \
+		--libexecdir=lib \
+		--localstatedir=/var \
+		-Dbroadway-backend=false \
+		-Dx11-backend=false \
+		-Dwayland-backend=true \
+		-Dwin32-backend=false \
+		-Dcolord=disabled \
+		-Ddemos=false \
+		-Dgtk_doc=false \
+		-Dintrospection=disabled \
+		-Dbuild-tests=false \
+		-Dbuild-examples=false \
+		-Dmedia-gstreamer=disabled \
+		-Dvulkan=enabled \
+		-Dmedia-ffmpeg=enabled \
+		-Dgi-docgen:development_tests=false
+		
+	samu -C _build
+}
+
+package() {
+	cd $pkgname-$pkgver
+	cd _build
+	DESTDIR=$pkgdir samu install
+}
+
+backup() {
+	return
+}
+
+license() {
+	cd $pkgname-$pkgver
+	cat COPYING
+}
---
+++ snd/helvum/build.sh
@@ -0,0 +1,33 @@
+pkgname=helvum
+pkgver=0.3.4
+
+_clear_vendor_checksums() {
+	sed -i 's/\("files":{\)[^}]*/\1/' vendor/$1/.cargo-checksum.json
+}
+
+fetch() {
+	curl -L "https://gitlab.freedesktop.org/pipewire/$pkgname/-/archive/$pkgver/$pkgname-$pkgver.tar.bz2" -o $pkgname-$pkgver.tar.bz2
+	tar -xf $pkgname-$pkgver.tar.bz2
+	cd $pkgname-$pkgver
+	mkdir -p .cargo
+	cargo vendor > .cargo/config
+}
+
+build() {
+	cd $pkgname-$pkgver
+	cargo build --release --locked --all-features
+}
+
+package() {
+	cd $pkgname-$pkgver
+	install -Dm755 target/release/$pkgname $pkgdir/usr/bin/$pkgname
+}
+
+backup() {
+	return
+}
+
+license() {
+	cd $pkgname-$pkgver
+	cat LICENSE
+}
